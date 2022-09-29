b48f705a33e557ea572c33b7c6baf756ef3ff160
Dice <What is Email?>
2022-09-29 11:15:58 +0200

    Improved grid with diffrent height sizes

--- create_grid.py
+++ create_grid.py
@@ -6,6 +6,13 @@ import math
 files = [f for f in os.listdir('./input') if os.path.isfile(os.path.join('./input', f))]
 
 
+biggest_height = 0
+
+for f in files:
+    img = Image.open(os.path.join('./input', f))
+
+    biggest_height = max(biggest_height, img.height)
+
 columns = math.ceil(math.sqrt(len(files)))
 rows = math.ceil(len(files) / columns)
 
@@ -16,18 +23,25 @@ index = 0
 first = Image.open(os.path.join('./input', files[0]))
 
 width = first.width
-height = first.height
+height = biggest_height
 
 new_im = Image.new('RGB', (width * columns, height * rows))
 
+nexty = 0
+
 for y in range(columns):
+    biggest_height_row = 0
     for x in range(columns):
         if index >= len(files):
             break
         im = Image.open(os.path.join('./input', files[index]))
         #im.thumbnail((300,300))
-        new_im.paste(im, (x * width, y * height))
+        new_im.paste(im, (x * width, nexty))
         index += 1
+
+        biggest_height_row = max(biggest_height_row, im.height)
+
+    nexty += biggest_height_row
 
 
 current_directory = os.getcwd()
